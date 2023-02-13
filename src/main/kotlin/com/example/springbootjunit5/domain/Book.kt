7345a1d48f26ed9ac6f55a2d31e3857ad93f9953
sjparkk <sjparkk0820@naver.com>
2023-02-13 22:07:28 +0900

    - 도메인 추가

---
+++ src/main/kotlin/com/example/springbootjunit5/domain/Book.kt
@@ -0,0 +1,19 @@
+package com.example.springbootjunit5.domain
+
+import javax.persistence.Entity
+import javax.persistence.GeneratedValue
+import javax.persistence.GenerationType
+import javax.persistence.Id
+
+
+@Entity
+class Book(
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long,
+    val title: String,
+    val author: String
+) {
+
+}
