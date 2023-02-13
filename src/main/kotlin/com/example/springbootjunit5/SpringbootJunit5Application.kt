1f63479f52b5cd69843827a7ba3ec89d6b6b029e
sjparkk <sjparkk0820@naver.com>
2023-02-13 22:05:03 +0900

    - git init

---
+++ src/main/kotlin/com/example/springbootjunit5/SpringbootJunit5Application.kt
@@ -0,0 +1,11 @@
+package com.example.springbootjunit5
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringbootJunit5Application
+
+fun main(args: Array<String>) {
+    runApplication<SpringbootJunit5Application>(*args)
+}
---
+++ src/test/kotlin/com/example/springbootjunit5/SpringbootJunit5ApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.springbootjunit5
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringbootJunit5ApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
