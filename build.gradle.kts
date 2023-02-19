daaade783b6fb9adf3c5d85f5d678fff011bc68b
sjparkk <sjparkk0820@naver.com>
2023-02-19 17:50:01 +0900

    - h2 디비 설정 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -34,6 +34,9 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jdbc")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-validation")
+
+    runtimeOnly("com.h2database:h2")
+
 }
 
 tasks.withType<KotlinCompile> {
